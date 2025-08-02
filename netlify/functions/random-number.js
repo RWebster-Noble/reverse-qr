1867f1e0bf539fdba39084f382e27587d9ce3322
RWebster-Noble <27361135+RWebster-Noble@users.noreply.github.com>
2025-08-02 11:27:45 +0100

    Store number

--- netlify/functions/random-number.js
+++ netlify/functions/random-number.js
@@ -1,14 +1,21 @@
+import { getStore } from '@netlify/blobs';
+
 exports.handler = async (event) => {
     try {
+        const store = getStore({ name: 'numbers', consistency: 'strong' });
         // Generate random number between 1 and 100
         const randomNumber = Math.floor(Math.random() * 100) + 1;
         
+        const randomNumberResponse = {
+            number: randomNumber,
+            timestamp: new Date().toISOString()
+        };
+
+        await store.setJSON(randomNumber.toString(), randomNumberResponse);
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ 
-                number: randomNumber,
-                timestamp: new Date().toISOString()
-            })
+            body: JSON.stringify(randomNumberResponse)
         };
     } catch (error) {
         return {
